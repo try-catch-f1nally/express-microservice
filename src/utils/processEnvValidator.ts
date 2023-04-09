b7405646158a6103d5d9d3ad8eaf3737cb52170d
try-catch-f1nally <finally.catch.try@gmail.com>
2023-04-09 19:33:56 +0300

    Add autogeneration of errorMessage property in env var validation schema

--- src/utils/processEnvValidator.ts
+++ src/utils/processEnvValidator.ts
@@ -8,6 +8,19 @@ ajvFormats(ajv);
 ajvErrors(ajv);
 
 export function processEnvValidator<T>(schema: JSONSchema<T>) {
+  const defaults = {
+    errorMessage: {
+      properties: Object.keys(schema.properties as object).reduce(
+        (acc, curr) => ((acc[curr] = `invalid ${curr} variable`), acc),
+        {} as {[key: string]: string}
+      ),
+      required: (schema.required as string[]).reduce(
+        (acc, curr) => ((acc[curr] = `missing required ${curr} variable`), acc),
+        {} as {[key: string]: string}
+      )
+    }
+  };
+  schema = Object.assign(defaults, schema);
   if (!ajv.validate(schema, process.env)) {
     const errors = (ajv.errors || []).map((err) => err.message || 'Validation error');
     throw new Error(`Invalid environment variables provided: ${errors.join(', ')}`);
